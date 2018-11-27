82a0a938ccac56a7b4a773f4d24bc3faafd22768
REU\mgebre <Mikias.Gebre@fi.rohmeurope.com>
2018-11-27 16:02:32 +0200

    second commit

--- networkx.py
+++ networkx.py
@@ -7,9 +7,40 @@ with open ("test.txt", "r") as myfile:
     data=myfile.readlines()
 
 	
-values = []
+node_1 = []
+node_2 = []
+edge_weight = []
+
+
+	
+l = data[0::1]	
 d = str(data)
 d.split(',')
 
 
-print(len(data))
+
+m = []
+
+
+
+print("----------------------------------------------------")
+
+
+
+
+
+for nodein in list:
+	nodein.split()
+	for lists in nodein:
+		m.append(lists)
+		
+		
+	
+for nodes in node_1:
+	node_2.append(nodes)
+	
+	
+print('--------------------------')
+
+print node_1
+	
